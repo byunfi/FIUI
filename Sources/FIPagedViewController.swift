38508b7412c85ca52def985302321f294e71f705
byunfi <byunfi@icloud.com>
2019-02-19 11:13:48 +0800

    init

---
+++ Sources/FIPagedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FIPagedViewController.swift
+//  FIUI
+//
+//  Created by 白云飞 on 2019/2/19.
+//  Copyright © 2019 白云飞. All rights reserved.
+//
+
+import UIKit
+
+class FIPagedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
